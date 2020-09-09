84b997e993302698f025af6b334215ec03747ffa
ShotAlex <shot.aleks@gmail.com>
2020-09-09 09:34:07 +0300

    fix: cats task and add some solutoin for carbon=dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; //of 15 dpm/g
+const HALF_LIFE_PERIOD = 5730; // The half-life for this reaction is 5700 ± 30 yr.
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function dateSample(sampleActivity) {
+  if ((typeof sampleActivity) !== 'string') return false;
+  if (+sampleActivity < 1) return false;
+  let answer = Math.round(+sampleActivity)
+
+  let chisl = Math.log(MODERN_ACTIVITY / sampleActivity);
+  let znam = HALF_LIFE_PERIOD;
+
+  
+  answer = chisl / znam;
+  return answer;
 };
