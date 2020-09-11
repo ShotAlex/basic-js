ee8e61815cec6df1657abde17107f8561f4b8448
ShotAlex <shot.aleks@gmail.com>
2020-09-11 22:17:34 +0300

    feat: added solution of the transform-array task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,10 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
-  // throw new CustomError('Not implemented');
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = Math.floor(turns / turnsSpeed * 3600);
-
 
   return { turns, seconds}
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  let answer = []
+  let checkNum;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (arr[i]) {
+      case '--discard-next': 
+        checkNum = i + 1;
+        i++;
+        break;
+        
+      case '--discard-prev': 
+        if (answer.length && checkNum != i-1) answer.pop();
+        break;
+        
+      case '--double-next': 
+        if (i < arr.length - 1) answer.push(arr[i + 1]);
+        break;
+        
+      case '--double-prev': 
+        if (answer.length && checkNum !== i-1) answer.push(arr[i - 1]);
+        break;
+        
+      default: answer.push(arr[i]);
+        break;
+    }
+  } 
+
+  return answer;
 };
