28f912a47644141a47e6a73d79cf387bc55cbbd8
ShotAlex <shot.aleks@gmail.com>
2020-09-12 11:04:11 +0300

    feat: added sulition of the recursive-depth tastk

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  calculateDepth( arr ) {
+    return Array.isArray(arr) 
+      ? 1 + Math.max(...arr.map( el => {
+        let count = this.calculateDepth(el);
+        return !isFinite(count) ? 1 : count;
+      })) 
+      : 0;
   }
+
 };
