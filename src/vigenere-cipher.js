3c41754c90f6d8a140b6765ff5de64504fbd4a1a
ShotAlex <shot.aleks@gmail.com>
2020-09-14 21:26:46 +0300

    feat: addded solution of the vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(ciphering) {
+    if (ciphering === false) this.mode = true
+    this.encrypt = this.encrypt.bind(this)
+    this.decrypt = this.decrypt.bind(this)
+  }
+
+  encrypt = (message, key) => this.cipher(message, key, 'encrypt')
+  decrypt = (message, key) => this.cipher(message, key, 'decrypt')
+
+
+  cipher(message, key, whatMethod) {
+    if (message === undefined || key === undefined) throw new Error()
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    let counter = 0
+
+    const answer = message.split('').map( ( char, idx) => {
+      if (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90) return char.charCodeAt(0)
+      if (whatMethod === 'decrypt') return (char.charCodeAt(0) - key.charCodeAt( counter++ % key.length ) + 26) % 26 + 65
+      return (char.charCodeAt(0) + key.charCodeAt(counter++ % key.length)) % 26 + 65
+    });
+    
+    if( this.mode) answer.reverse()
+    return String.fromCharCode( ...answer )
   }
 }
 
